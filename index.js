4d8b48872850cd1e532b96834eac2e67324a9ae9
Caroline <caroline.cps009@gmail.com>
2025-10-22 22:28:05 -0300

    quantidade alterada

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 async function loadGallery() {
 const gallery = document.getElementById('gallery');
-const url = 'https://pokeapi.co/api/v2/pokemon?limit=60';
+const url = 'https://pokeapi.co/api/v2/pokemon?limit=125';
 const response = await fetch(url);
 const Pokemon = await response.json();
 console.log (Pokemon);
